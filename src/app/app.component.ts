eb619d9b41b1af9e469453c93b9a102e4e8ad40c
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-01-07 16:01:33 -0300

    Prueba1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from "./heroes/heroes.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
