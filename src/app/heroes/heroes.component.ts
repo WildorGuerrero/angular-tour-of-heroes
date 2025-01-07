9df6aadd2085ef20df8ee7914401e32a47b00564
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-01-07 16:36:24 -0300

    Posible Arreglo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  imports: [CommonModule, FormsModule]
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  
+
   constructor() { }
 
   ngOnInit() {
